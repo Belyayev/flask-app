64b096606426dd08d3d86180d353e2c366071d53
Mike Belyayev <4xgood@gmail.com>
2024-08-25 12:49:27 -0600

    need a fix

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
     name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
--- run.py
+++ run.py
@@ -1,16 +1,43 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc://SA:YourStrong!Passw0rd@host.docker.internal:1433/your_database?driver=ODBC+Driver+17+for+SQL+Server"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
-# Example route
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def index():
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
+@app.route('/add', methods=['POST'])
+def add_user():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    new_user = User(name=name, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:id>', methods=['POST'])
+def update_user(id):
+    user = User.query.get(id)
+    user.name = request.form.get('name')
+    user.email = request.form.get('email')
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete_user(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
